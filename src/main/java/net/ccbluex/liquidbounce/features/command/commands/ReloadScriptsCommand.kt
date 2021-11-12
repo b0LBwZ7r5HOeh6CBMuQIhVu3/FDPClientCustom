663b065bf16dab883725c41b6503f9d8eb5f47da
ll11l1lIllIl1lll <helloworldsys@163.com>
2021-11-12 21:04:43 +0800

    Create ReloadScriptsCommand.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/ReloadScriptsCommand.kt
@@ -0,0 +1,40 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/UnlegitMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.command.commands
+
+import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.features.command.Command
+import net.ccbluex.liquidbounce.features.command.CommandManager
+import net.ccbluex.liquidbounce.features.module.modules.misc.KillInsults
+import net.ccbluex.liquidbounce.ui.cape.GuiCapeManager
+import net.ccbluex.liquidbounce.ui.font.Fonts
+
+class ReloadCommand : Command("reload", emptyArray()) {
+    /**
+     * Execute commands with provided [args]
+     */
+    override fun execute(args: Array<String>) {
+        alert("Reloading...")
+                LiquidBounce.commandManager = CommandManager()
+                LiquidBounce.commandManager.registerCommands()
+                LiquidBounce.isStarting = true
+                LiquidBounce.scriptManager.disableScripts()
+                LiquidBounce.scriptManager.unloadScripts()
+                for(module in LiquidBounce.moduleManager.modules)
+                    LiquidBounce.moduleManager.generateCommand(module)
+                LiquidBounce.scriptManager.loadScripts()
+                LiquidBounce.scriptManager.enableScripts()
+            LiquidBounce.configManager.load(LiquidBounce.configManager.nowConfig, false)
+                LiquidBounce.isStarting = false
+
+                LiquidBounce.clickGui = ClickGui()
+                LiquidBounce.fileManager.loadConfig(LiquidBounce.fileManager.clickGuiConfig)
+        alert("Reloaded.")
+        LiquidBounce.isStarting = false
+        LiquidBounce.isLoadingConfig = false
+        System.gc()
+    }
+}
