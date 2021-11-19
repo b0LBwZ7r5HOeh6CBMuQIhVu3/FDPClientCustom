fe772b55bee36ff9697be41f0305a4dee56f5337
ll11l1lIllIl1lll <helloworldsys@163.com>
2021-11-19 22:23:50 +0800

    A

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Fly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Fly.kt
@@ -14,6 +14,7 @@ import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.network.play.server.S19PacketEntityStatus
 import org.lwjgl.input.Keyboard
 import java.awt.Color
+import kotlin.math.sqrt
 
 @ModuleInfo(name = "Fly", category = ModuleCategory.MOVEMENT, autoDisable = EnumAutoDisableType.FLAG, keyBind = Keyboard.KEY_F)
 class Fly : Module() {
