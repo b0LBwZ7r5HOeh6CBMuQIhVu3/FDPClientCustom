869eb07ffd2510ba2bd6a54efbeec7d4ab778d46
ll11l1lIllIl1lll <helloworldsys@163.com>
2021-11-24 13:05:11 +0800

    A

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinNetworkPlayerInfo.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinNetworkPlayerInfo.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
+import java.util.Random;
 import java.util.Objects;
 
 @Mixin(NetworkPlayerInfo.class)
@@ -21,14 +21,15 @@ public class MixinNetworkPlayerInfo {
     @Shadow
     @Final
     private GameProfile gameProfile;
-
+    private static final ResourceLocation TEXTURE_LIULIAQUI = new ResourceLocation("fdpclient/skin/TEXTURE_LIULIAQUI.png");
+    private static final ResourceLocation TEXTURE_PIGEON233 = new ResourceLocation("fdpclient/skin/TEXTURE_PIGEON233.png");
     @Inject(method = "getLocationSkin", cancellable = true, at = @At("HEAD"))
     private void injectSkinProtect(CallbackInfoReturnable<ResourceLocation> cir) {
         NameProtect nameProtect = LiquidBounce.moduleManager.getModule(NameProtect.class);
 
         if (nameProtect.getState() && nameProtect.skinProtectValue.get()) {
             if (nameProtect.allPlayersValue.get() || Objects.equals(gameProfile.getId(), Minecraft.getMinecraft().getSession().getProfile().getId())) {
-                cir.setReturnValue(DefaultPlayerSkin.getDefaultSkin(this.gameProfile.getId()));
+                cir.setReturnValue(Random().nextBoolean() ? TEXTURE_LIULIAQUI : TEXTURE_PIGEON233);
                 cir.cancel();
             }
         }
