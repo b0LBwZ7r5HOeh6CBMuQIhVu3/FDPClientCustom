9d7d1962b88d336bf9ac183866167fef44b07395
ll11l1lIllIl1lll <helloworldsys@163.com>
2021-12-01 21:29:58 +0800

    A

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Criticals.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Criticals.kt
@@ -29,7 +29,7 @@ import net.minecraft.stats.StatList
 
 @ModuleInfo(name = "Criticals", category = ModuleCategory.COMBAT)
 class Criticals : Module() {
-val modeValue = ListValue("Mode", arrayOf("Packet", "NCPPacket", "Hypixel", "Hypixel2", "AACPacket","LitePacket", "AAC4.3.11OldHYT, "AAC5.0.4", "NoGround", "Visual", "TPHop", "FakeCollide", "Mineplex", "More", "TestMinemora", "Motion", "Hover", "Matrix"), "packet")
+val modeValue = ListValue("Mode", arrayOf("Packet", "NCPPacket", "Hypixel", "Hypixel2", "AACPacket","LitePacket", "AAC4.3.11OldHYT", "AAC5.0.14HYT", "NoGround", "Visual", "TPHop", "FakeCollide", "Mineplex", "More", "TestMinemora", "Motion", "Hover", "Matrix"), "packet")
     val motionValue = ListValue("MotionMode", arrayOf("RedeSkyLowHop", "Hop", "Jump", "LowJump", "MinemoraTest"), "Jump")
     val hoverValue = ListValue("HoverMode", arrayOf("AAC4", "AAC4Other", "OldRedesky", "Normal1", "Normal2", "Minis", "Minis2", "TPCollide", "2b2t","Edit"), "AAC4")
     val hoverNoFall = BoolValue("HoverNoFall", true)
@@ -102,7 +102,7 @@ val modeValue = ListValue("Mode", arrayOf("Packet", "NCPPacket", "Hypixel", "Hyp
                     sendCriticalPacket(yOffset = 0.00000000343, ground = false)
                 }
                 
-                "aac5.0.4" -> { //aac5.0.4 moment but with bad cfg(cuz it will flag for timer)
+                "AAC5.0.14HYT" -> { //AAC5.0.14HYT moment but with bad cfg(cuz it will flag for timer)
                     sendCriticalPacket(yOffset = 0.00133545, ground = false)
                     sendCriticalPacket(yOffset = -0.000000433, ground = false)
                 }
