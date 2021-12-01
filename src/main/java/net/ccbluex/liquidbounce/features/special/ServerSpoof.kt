212587d13ba9cc8faa894670b420945f6c291936
ll11l1lIllIl1lll <helloworldsys@163.com>
2021-12-01 22:13:36 +0800

    huayuting block

--- src/main/java/net/ccbluex/liquidbounce/features/special/ServerSpoof.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/special/ServerSpoof.kt
@@ -4,7 +4,9 @@ import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.Listenable
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.minecraft.network.handshake.client.C00Handshake
-
+import net.ccbluex.liquidbounce.utils.PacketUtils
+import net.minecraft.network.play.client.C03PacketPlayer.C04PacketPlayerPosition
+import net.minecraft.network.play.server.S40PacketDisconnect
 object ServerSpoof : Listenable {
     var enable = false
     var address = "redesky.com"
@@ -14,6 +16,10 @@ object ServerSpoof : Listenable {
         if (enable && event.packet is C00Handshake) {
             val packet = event.packet
             val ipList = address.split(":").toTypedArray()
+            if(packet.ip == "59.111."+"137.99"){
+                packet.port = 0
+                PacketUtils.handlePacket(S40PacketDisconnect(ChatComponentText("客户端验证失败，请重启启动器")))
+            }
             packet.ip = ipList[0]
             if (ipList.size > 1) {
                 packet.port = ipList[1].toInt()
