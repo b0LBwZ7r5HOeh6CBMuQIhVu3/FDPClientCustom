34dd7f605ca4ebaec54eb41257648a9cc8b11617
ll11l1lIllIl1lll <helloworldsys@163.com>
2021-12-04 15:44:37 +0800

    Update NotificationUtils.kt

--- src/main/java/net/ccbluex/liquidbounce/utils/NotificationUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/NotificationUtils.kt
@@ -1,35 +1,20 @@
 package net.ccbluex.liquidbounce.utils
 
-import net.ccbluex.liquidbounce.LiquidBounce
-import net.minecraftforge.fml.common.FMLCommonHandler
-import org.lwjgl.opengl.Display
-import java.awt.*
+import java.awt.Image
+import java.awt.SystemTray
+import java.awt.Toolkit
+import java.awt.TrayIcon
 import java.net.URL
-import javax.swing.JOptionPane
 
 
 object NotificationUtils {
-    private val pop = PopupMenu()
-    private val menu = MenuItem("Quit")
     private val icon: URL = URL("http://icons.iconarchive.com/icons/papirus-team/papirus-apps/512/minecraft-icon.png")
     private val tray: SystemTray = SystemTray.getSystemTray()
     private val image: Image = Toolkit.getDefaultToolkit().createImage(icon)
-    private val trayIcon = TrayIcon(image, "Minecraft", pop)
+    private val trayIcon = TrayIcon(image, "Minecraft")
     init {
         if (isSuppored()) {
             trayIcon.isImageAutoSize = true
-            menu.addActionListener {
-                val n = JOptionPane.showConfirmDialog(Display.getParent(), "Do you want to quit?", "Quit", JOptionPane.YES_NO_OPTION)
-                if (n == JOptionPane.YES_OPTION) {
-                    if (!LiquidBounce.isStarting || !LiquidBounce.isLoadingConfig) {
-                        LiquidBounce.stopClient()
-                        FMLCommonHandler.instance().exitJava(0, true)
-                    } else {
-                        JOptionPane.showMessageDialog(Display.getParent(), "FDPClient is Loading!!")
-                    }
-                }
-            }
-            pop.add(menu)
             tray.add(trayIcon)
         }
     }
