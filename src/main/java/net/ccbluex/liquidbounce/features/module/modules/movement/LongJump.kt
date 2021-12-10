800144c11e2e26517acf5005697fe17050b66599
ll11l1lIllIl1lll <helloworldsys@163.com>
2021-12-11 01:13:40 +0800

    A

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/LongJump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/LongJump.kt
@@ -269,7 +269,7 @@ class LongJump : Module() {
                     "hyt4v4" -> {
                         mc.thePlayer.motionY += 0.031470000997
                         MovementUtils.strafe(MovementUtils.getSpeed() * 1.0114514f)
-                        mc.timer.timerSpeed = 0.8f
+                        if(mc.thePlayer.motionY > 0) mc.timer.timerSpeed = 0.8f else mc.timer.timerSpeed = 1.0f
                     }
                 }
             }
