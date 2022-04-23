2ff29b0e93a170ac4cb25f4805f45f8b10affd02
ll11l1lIllIl1lll <helloworldsys@163.com>
2022-04-23 13:07:03 +0800

    idfk

--- src/main/java/net/ccbluex/liquidbounce/utils/render/AnimationUtils.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/AnimationUtils.java
@@ -6,7 +6,24 @@
 package net.ccbluex.liquidbounce.utils.render;
 
 public class AnimationUtils {
+    public static float lstransition(float now, float desired, double speed) {
+        final double dif = Math.abs(desired - now);
+        float a = (float) Math.abs((desired - (desired - (Math.abs(desired - now)))) / (100 - (speed * 10)));
+        float x = now;
 
+        if (dif > 0) {
+            if (now < desired)
+                x += a * RenderUtils.deltaTime;
+            else if (now > desired)
+                x -= a * RenderUtils.deltaTime;
+        } else
+            x = desired;
+
+        if(Math.abs(desired - x) < 10.0E-3 && x != desired)
+            x = desired;
+
+        return x;
+    }
     /**
      * In-out-easing function
      * https://github.com/jesusgollonet/processing-penner-easing
