a370fb858885bbd9cedb85729e075c0964880ba8
ll11l1lIllIl1lll <helloworldsys@163.com>
2023-01-08 17:13:23 +0800

    A

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/ClipFly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/ClipFly.kt
@@ -5,27 +5,33 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.ccbluex.liquidbounce.utils.timer.MSTimer
 import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.BoolValue
 import kotlin.math.cos
 import kotlin.math.sin
 
 class ClipFly : FlyMode("Clip") {
-    private val xValue = FloatValue("${valuePrefix}-X", 2f, -5f, 5f)
-    private val yValue = FloatValue("${valuePrefix}-Y", 2f, -5f, 5f)
-    private val zValue = FloatValue("${valuePrefix}-Z", 2f, -5f, 5f)
-    private val delayValue = IntegerValue("${valuePrefix}-Delay", 500, 0, 3000)
-    private val motionXValue = FloatValue("${valuePrefix}-MotionX", 0f, -1f, 1f)
-    private val motionYValue = FloatValue("${valuePrefix}-MotionY", 0f, -1f, 1f)
-    private val motionZValue = FloatValue("${valuePrefix}-MotionZ", 0f, -1f, 1f)
+    private val xValue = FloatValue("${valuePrefix}-X", 5f, -5f, 5f)
+    private val yValue = FloatValue("${valuePrefix}-Y", -2f, -5f, 5f)
+    private val zValue = FloatValue("${valuePrefix}-Z", 5f, -5f, 5f)
+    private val delayValue = IntegerValue("${valuePrefix}-Delay", 1000, 0, 3000)
+    private val timerValue = FloatValue("${valuePrefix}-Timer", 0.7f, 0.2f, 1f)
+    private val packetOnGroundValue = BoolValue("${valuePrefix}packetOnGround", true)
 
     private val timer = MSTimer()
 
     override fun onEnable() {
         timer.reset()
     }
+    override fun onPacket(event: PacketEvent) {
+        val packet = event.packet
 
+        if (packet is C03PacketPlayer) {
+            packet.onGround = packetOnGroundValue.get()
+        }
+    }
     override fun onUpdate(event: UpdateEvent) {
         mc.thePlayer.onGround = false
-        mc.timer.timerSpeed = 0.7f
+        mc.timer.timerSpeed = timerValue.get()
         mc.thePlayer.motionX = 0.0
         mc.thePlayer.motionY = 0.0
         mc.thePlayer.motionZ = 0.0
